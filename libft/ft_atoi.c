485808bad5f118090001602641cf9af7bf1548fd
Yacine Hadari <yhadari@e2r2p10.1337.ma>
2019-10-25 19:10:15 +0200

    modify

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,26 +6,17 @@
 /*   By: yhadari <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 15:49:27 by yhadari           #+#    #+#             */
-/*   Updated: 2019/10/21 14:43:01 by yhadari          ###   ########.fr       */
+/*   Updated: 2019/10/25 19:09:19 by yhadari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int		longtest(unsigned long result, int j)
-{
-	if (j < 0 && (result - 1) > 4294967295)
-		return (0);
-	if (j > 0 && result > 4294967295)
-		return (-1);
-	return (result * j);
-}
-
 int				ft_atoi(const char *str)
 {
-	unsigned long	result;
-	int				i;
-	int				j;
+	int	result;
+	int	i;
+	int	j;
 
 	i = 0;
 	j = 1;
@@ -43,5 +34,5 @@ int				ft_atoi(const char *str)
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
-	return (longtest(result, j));
+	return (result * j);
 }
